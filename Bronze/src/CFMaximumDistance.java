ef22a5af23e7e56036460375cdbdef996e5d9a18
ethan7sky <ethan7sky@gmail.com>
2022-09-02 16:07:34 -0700

    Bronze

--- Bronze/src/CFMaximumDistance.java
+++ Bronze/src/CFMaximumDistance.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class CFMaximumDistance {
 	
 	static Scanner in;
-	static int N, x[], y[];
+	static int N, x[], y[], max;
 	
 	public static void main(String[] args) {
 		
@@ -13,11 +13,6 @@ public class CFMaximumDistance {
 		x = new int[N];
 		y = new int[N];
 		
-		int[] l = new int[] {Integer.MAX_VALUE,Integer.MAX_VALUE};
-		int[] r = new int[] {Integer.MIN_VALUE,Integer.MIN_VALUE};
-		int[] u = new int[] {Integer.MIN_VALUE,Integer.MIN_VALUE};
-		int[] d = new int[] {Integer.MAX_VALUE,Integer.MAX_VALUE};
-		
 		for(int i = 0; i < N; i++) {
 			x[i] = in.nextInt();
 		}
@@ -25,45 +20,16 @@ public class CFMaximumDistance {
 			y[i] = in.nextInt();
 		}
 		
+		max = 0;
 		for(int i = 0; i < N; i++) {
-			
-			if(x[i] < l[0] || x[i] == l[0] && y[i] < l[1]) {
-				l[0] = x[i];
-				l[1] = y[i];
-			}
-			if(x[i] > r[0] || x[i] == r[0] && y[i] > r[1]) {
-				r[0] = x[i];
-				r[1] = y[i];
-			}
-			if(y[i] > u[1] || y[i] == u[1] && x[i] < u[0]) {
-				u[0] = x[i];
-				u[1] = y[i];
-			}
-			if(y[i] < d[1]|| y[i] == u[1] && x[i] > u[0]) {
-				d[0] = x[i];
-				d[1] = y[i];
+			for(int j = i+1; j < N; j++) {
+				int len1 = x[i]-x[j];
+				int len2 = y[i]-y[j];
+				
+				max = Math.max(max, len1*len1+len2*len2);
 			}
 		}
 		
-		
-		long max = Long.MIN_VALUE;
-		
-		max = Math.max(max, solve(l, u));
-		max = Math.max(max, solve(u, r));
-		max = Math.max(max, solve(r, d));
-		max = Math.max(max, solve(d, l));
-		max = Math.max(max, solve(u, d));
-		max = Math.max(max, solve(l, r));
-		
-		
-		System.out.println(max);
-		
-		
-	}
-	
-	static long solve(int[] a, int[] b) {
-		long rise = (long) b[1]- (long) a[1];
-		long run = (long) b[0]- (long) a[0];
-		return (long) ( Math.pow(rise, 2) + Math.pow(run, 2) );
+		System.out.println(max);		
 	}
 }
--- Bronze/src/train_usaco_FridayTheThirteenth.java
+++ Bronze/src/train_usaco_FridayTheThirteenth.java
@@ -10,7 +10,7 @@ public class train_usaco_FridayTheThirteenth {
 	
 	static Scanner in;
 	static PrintWriter out;
-	
+	 
 	public static void main(String[] args) throws IOException {
 		
 		//in = new Scanner(System.in);
