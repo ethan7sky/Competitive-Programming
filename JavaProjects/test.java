e8bb76e3757edfe03bc1d3f6e7707c8b5bdfa107
Ethan Lee <ethan7sky@gmail.com>
2025-05-30 23:47:21 -0700

    new laptop

---
+++ JavaProjects/test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class test {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println("Hello World " + n);
+        in.close();
+    }   
+}
